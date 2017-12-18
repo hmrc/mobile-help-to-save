f965c59ea200b9f5117578b185567e980d9c9065
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2017-12-18 14:42:18 +0000

    NGC-2634 Bump scalatest dependency for consistency with version used by hmrctest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ object AppDependencies {
 
   def test(scope: String = "test,it") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
-    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.4" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
   )
