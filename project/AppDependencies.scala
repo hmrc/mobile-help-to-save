9330ef467b02f4b1f292c6db3148ffd4a8310898
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2018-03-02 15:53:04 +0000

    Upgrade play-reactivemongo
    ...to avoid MongoDB "Fails to update history" NoSuchElementException errors in logs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-play-25" % "1.4.0",
     "uk.gov.hmrc" %% "auth-client" % "2.5.0",
     "uk.gov.hmrc" %% "domain" % "5.1.0",
-    "uk.gov.hmrc" %% "play-reactivemongo" % "6.1.0",
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0",
     "org.typelevel" %% "cats-core" % "1.0.1"
   )
 
