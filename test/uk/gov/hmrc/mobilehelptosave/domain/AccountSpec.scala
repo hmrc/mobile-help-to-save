beef30b7aee029f1b83c16ab8171122ee2770f8a
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2018-08-31 13:15:17 +0100

    [NGC-3740][FD] Improve nextPaymentMonthStartDate unit test

--- test/uk/gov/hmrc/mobilehelptosave/domain/AccountSpec.scala
+++ test/uk/gov/hmrc/mobilehelptosave/domain/AccountSpec.scala
@@ -16,6 +16,7 @@
 
 package uk.gov.hmrc.mobilehelptosave.domain
 
+import org.joda.time.{LocalDate, YearMonth}
 import org.scalatest.{Matchers, WordSpec}
 import uk.gov.hmrc.mobilehelptosave.AccountTestData
 
@@ -23,13 +24,52 @@ class AccountSpec extends WordSpec with Matchers
   with AccountTestData {
 
   "apply" should {
+    "include nextPaymentMonthStartDate when next month will start before the end of the the last bonus term" in {
+      val penultimateMonthHelpToSaveAccount = helpToSaveAccount.copy(
+        openedYearMonth = new YearMonth(2018, 1),
+        thisMonthEndDate = new LocalDate(2021, 11, 30),
+        bonusTerms = Seq(
+          BonusTerm(
+            bonusEstimate = BigDecimal("90.99"),
+            bonusPaid = BigDecimal("90.99"),
+            endDate = new LocalDate(2019, 12, 31),
+            bonusPaidOnOrAfterDate = new LocalDate(2020, 1, 1)
+          ),
+          BonusTerm(
+            bonusEstimate = 12,
+            bonusPaid = 0,
+            endDate = new LocalDate(2021, 12, 31),
+            bonusPaidOnOrAfterDate = new LocalDate(2022, 1, 1)
+          )
+        )
+      )
+
+      val account = Account(penultimateMonthHelpToSaveAccount)
+      account.nextPaymentMonthStartDate shouldBe Some(new LocalDate(2021, 12, 1))
+    }
+
     "omit nextPaymentMonthStartDate when payments will not be possible next month because it will be after the last bonus term" in {
-      val thisMonthEndDate = helpToSaveAccount.bonusTerms.last.endDate
-      val lastMonthHelpToSaveAccount = helpToSaveAccount.copy(thisMonthEndDate = thisMonthEndDate)
+      val lastMonthHelpToSaveAccount = helpToSaveAccount.copy(
+        openedYearMonth = new YearMonth(2018, 1),
+        thisMonthEndDate = new LocalDate(2021, 12, 31),
+        bonusTerms = Seq(
+          BonusTerm(
+            bonusEstimate = BigDecimal("90.99"),
+            bonusPaid = BigDecimal("90.99"),
+            endDate = new LocalDate(2019, 12, 31),
+            bonusPaidOnOrAfterDate = new LocalDate(2020, 1, 1)
+          ),
+          BonusTerm(
+            bonusEstimate = 12,
+            bonusPaid = 0,
+            endDate = new LocalDate(2021, 12, 31),
+            bonusPaidOnOrAfterDate = new LocalDate(2022, 1, 1)
+          )
+        )
+      )
 
       val account = Account(lastMonthHelpToSaveAccount)
       account.nextPaymentMonthStartDate shouldBe None
-      account shouldBe mobileHelpToSaveAccount.copy(thisMonthEndDate = thisMonthEndDate, nextPaymentMonthStartDate = None)
     }
   }
 }
