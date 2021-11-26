4854dd0899fc7883d99ca8fe21ab49b609eb52f5
aerodigi <54714520+aerodigi@users.noreply.github.com>
2021-11-26 15:22:39 +0000

    [HMA-5102] Update IT to improve milestone dates

--- it/uk/gov/hmrc/mobilehelptosave/MilestonesISpec.scala
+++ it/uk/gov/hmrc/mobilehelptosave/MilestonesISpec.scala
@@ -444,7 +444,7 @@ class MilestonesISpec
         .as[String]                                                    shouldBe "EndOfFinalBonusPeriodZeroBalancePositiveBonus"
       (response.json \ "milestones" \ 0 \ "milestoneTitle").as[String] shouldBe "It's nearly the end of year 4"
       (response.json \ "milestones" \ 0 \ "milestoneMessage")
-        .as[String] shouldBe s"Your final bonus of £250 will be paid into your bank account by ${secondEndDate.plusDays(1).format(dateFormat)}."
+        .as[String] shouldBe s"Your final bonus of £250 will be paid into your bank account by ${secondEndDate.plusDays(10).format(dateFormat)}."
     }
 
     "respond with 200 and the EndOfFinalBonusPeriodPositiveBalanceNoBonus milestone in a list as JSON when the milestone is hit" in {
@@ -463,7 +463,7 @@ class MilestonesISpec
         .as[String]                                                    shouldBe "EndOfFinalBonusPeriodPositiveBalanceNoBonus"
       (response.json \ "milestones" \ 0 \ "milestoneTitle").as[String] shouldBe "It's nearly the end of year 4"
       (response.json \ "milestones" \ 0 \ "milestoneMessage")
-        .as[String] shouldBe s"Your savings of £1350 will be paid into your bank account by ${secondEndDate.plusDays(1).format(dateFormat)}."
+        .as[String] shouldBe s"Your savings of £1350 will be paid into your bank account by ${secondEndDate.plusDays(10).format(dateFormat)}."
     }
 
     "respond with 200 and the EndOfFinalBonusPeriodPositiveBalancePositiveBonus milestone in a list as JSON when the milestone is hit" in {
@@ -482,7 +482,7 @@ class MilestonesISpec
         .as[String]                                                    shouldBe "EndOfFinalBonusPeriodPositiveBalancePositiveBonus"
       (response.json \ "milestones" \ 0 \ "milestoneTitle").as[String] shouldBe "It's nearly the end of year 4"
       (response.json \ "milestones" \ 0 \ "milestoneMessage")
-        .as[String] shouldBe s"Your savings of £1350 and final bonus of £600 will be paid into your bank account by ${secondEndDate.plusDays(1).format(dateFormat)}."
+        .as[String] shouldBe s"Your savings of £1350 and final bonus of £600 will be paid into your bank account by ${secondEndDate.plusDays(10).format(dateFormat)}."
     }
 
     "respond with 200 and the FinalBonusEarnedMaximum milestone in a list as JSON when the milestone is hit" in {
