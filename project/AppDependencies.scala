31505373487d76a9a540b6e2aaf921f3745bdef9
Martin Rolfe <52818407+mrolfe88@users.noreply.github.com>
2023-12-06 09:59:23 +0000

    [HMA-8019] Fix PR Bot comments (#259)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,17 +8,18 @@ object AppDependencies {
     resolvers += "emueller-bintray" at "https://dl.bintray.com/emueller/maven"
   )
 
-  private val hmrcMongoVersion  = "0.74.0"
-  private val bootstrapVersion  = "8.1.0"
-  private val domainVersion     = "8.3.0-play-28"
-  private val catsCoreVersion   = "2.9.0"
-  private val catsParVersion    = "1.0.0-RC2"
-  private val scalaUriVersion   = "1.5.1"
-  private val playHmrcVersion   = "7.2.0-play-28"
-  private val enumeratumVersion = "1.8.0"
-  private val macrosVersion     = "2.5.9"
-  private val refinedVersion    = "0.9.26"
-  private val htsKalcVersion    = "0.7.0"
+  private val hmrcMongoVersion     = "0.74.0"
+  private val bootstrapVersion     = "8.1.0"
+  private val domainVersion        = "8.3.0-play-28"
+  private val catsCoreVersion      = "2.9.0"
+  private val catsParVersion       = "1.0.0-RC2"
+  private val scalaUriVersion      = "1.5.1"
+  private val playHmrcVersion      = "7.2.0-play-28"
+  private val enumeratumVersion    = "1.8.0"
+  private val macrosVersion        = "2.5.9"
+  private val refinedVersion       = "0.9.26"
+  private val htsKalcVersion       = "0.7.0"
+  private val jacksonModuleVersion = "2.14.2"
 
   private val scalaMockVersion = "5.1.0"
 
@@ -27,18 +28,19 @@ object AppDependencies {
   private val playJsonExtensionsVersion      = "0.42.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"              %% "bootstrap-backend-play-28"  % bootstrapVersion,
-    "uk.gov.hmrc"              %% "domain"                     % domainVersion,
-    "org.typelevel"            %% "cats-core"                  % catsCoreVersion,
-    "io.chrisdavenport"        %% "cats-par"                   % catsParVersion,
-    "io.lemonlabs"             %% "scala-uri"                  % scalaUriVersion,
-    "uk.gov.hmrc"              %% "play-hmrc-api"              % playHmrcVersion,
-    "uk.gov.hmrc.mongo"        %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
-    "com.beachape"             %% "enumeratum-play-json"       % enumeratumVersion,
-    "com.softwaremill.macwire" %% "macros"                     % macrosVersion,
-    "eu.timepit"               %% "refined"                    % refinedVersion,
-    "uk.gov.hmrc"              % "help-to-save-kalculator-jvm" % htsKalcVersion,
-    "ai.x"                     %% "play-json-extensions"       % playJsonExtensionsVersion
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"  % bootstrapVersion,
+    "uk.gov.hmrc"                  %% "domain"                     % domainVersion,
+    "org.typelevel"                %% "cats-core"                  % catsCoreVersion,
+    "io.chrisdavenport"            %% "cats-par"                   % catsParVersion,
+    "io.lemonlabs"                 %% "scala-uri"                  % scalaUriVersion,
+    "uk.gov.hmrc"                  %% "play-hmrc-api"              % playHmrcVersion,
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
+    "com.beachape"                 %% "enumeratum-play-json"       % enumeratumVersion,
+    "com.softwaremill.macwire"     %% "macros"                     % macrosVersion,
+    "eu.timepit"                   %% "refined"                    % refinedVersion,
+    "uk.gov.hmrc"                  % "help-to-save-kalculator-jvm" % htsKalcVersion,
+    "ai.x"                         %% "play-json-extensions"       % playJsonExtensionsVersion,
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % jacksonModuleVersion
   )
 
   val test: Seq[ModuleID] = testCommon("test") ++ Seq(
@@ -48,10 +50,9 @@ object AppDependencies {
   val integrationTest: Seq[ModuleID] = testCommon("it") ++ Seq.empty
 
   def testCommon(scope: String): Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"     % bootstrapVersion               % scope,
-    "org.pegdown"                  % "pegdown"                     % pegdownVersion                 % scope,
-    "com.eclipsesource"            %% "play-json-schema-validator" % playJsonSchemaValidatorVersion % scope,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.14.2"                       % scope,
+    "uk.gov.hmrc"       %% "bootstrap-test-play-28"     % bootstrapVersion               % scope,
+    "org.pegdown"       % "pegdown"                     % pegdownVersion                 % scope,
+    "com.eclipsesource" %% "play-json-schema-validator" % playJsonSchemaValidatorVersion % scope,
     // workaround for version clash in IntelliJ where without this line both jetty-util-9.2.15.v20160210 and jetty-util-9.2.22.v20170606 are brought in
     // which results in a NoSuchMethodError when running StartupISpec
     "org.eclipse.jetty.websocket" % "websocket-client" % "9.2.22.v20170606" % scope
